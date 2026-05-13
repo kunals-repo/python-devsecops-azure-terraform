efc4cfa90249605cdd9938ff98ae2562295af872
kunals-repo <kunalpatil9324@gmail.com>
2026-05-13 23:47:50 +0530

    remove providers block from main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,24 +1,4 @@
 
-// Providers block added :
-
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-      version = "~>4.61.0"
-    } 
-  }
-  
-  required_version = "~> 1.14.5"   // version changed from 1.8.5 to 1.14.5 
-}
-
-provider "azurerm" {
-   features {
-
-   }
-  # Configuration options
-}
-
 resource "azurerm_resource_group" "rg-block" {
   name = "linux-vm-rg"
   location = "centralus"
