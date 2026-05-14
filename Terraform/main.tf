71e7353c66b6f0380e2288b4ba1222b2250e0785
kunals-repo <kunalpatil9324@gmail.com>
2026-05-14 07:42:45 +0530

    linux vm location & sku size change

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_resource_group" "rg-block" {
   name = "linux-vm-rg"
-  location = "centralus"
+  location = "eastus"
 }
 
 resource "azurerm_virtual_network" "vnet-block" {
@@ -64,7 +64,7 @@ resource "azurerm_linux_virtual_machine" "linux-vm-block" {
   name                = "linux-vm"
   resource_group_name = azurerm_resource_group.rg-block.name
   location            = azurerm_resource_group.rg-block.location
-  size                = "Standard_F2"
+  size                = "Standard_DC1s_v3"
   admin_username      = "kunal"
   network_interface_ids = [
     azurerm_network_interface.nic-card.id,
