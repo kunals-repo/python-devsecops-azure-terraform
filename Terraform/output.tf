3e816ebe6eba70335a66cc604c6fb9f84b8dadcd
kunals-repo <kunalpatil9324@gmail.com>
2026-05-15 16:17:31 +0530

    Updated output.tf

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -1,9 +1,9 @@
 output "vm_public_ip" {
     value = azurerm_linux_virtual_machine.linux-vm-block.public_ip_address
-    description = "This will output vm public ip & store in github secret"
+    description = "This will output vm public ip & stored as step level github output variable"
 }
 
 output "vm_hostname" {
     value = azurerm_linux_virtual_machine.linux-vm-block.admin_username
-    description = "This will output vm hostname & store in github secret"
+    description = "This will output vm hostname & stored as step level github output variable"
 }
